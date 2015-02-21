797ccd0381ba992b7a86db9a500f6269350a56c5
lokesh-webonise <lokesh@weboniselab.com>
2015-02-21 16:11:13 +0530

    initial commit

---
+++ Berksfile
@@ -0,0 +1,6 @@
+source "https://supermarket.chef.io"
+
+metadata
+
+cookbook "mysql"
+cookbook "nginx", "~> 2.6"
---
+++ recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: test
+# Recipe:: default
+#
+# Copyright (C) 2015 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+execute "create file" do
+	command "echo "Hello" > /tmp/test"
+end
